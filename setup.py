af8cbf8ecba0021602897b58fced50ace158acd6
Randall Degges <rdegges@gmail.com>
2010-11-15 15:12:43 -0800

    Adding a setuptools deploy script.
    This script will allow:
    
    	1. The project to be registered at PyPI (the CPAN of python).
    	2. The project to automatically install all dependencies via PyPI.
    	3. The project to declare itself as version 1.0.
    	4. The project to include it's pyhackerstories library so that it can run
    	   appropriately.
    	5. The project to install the script 'hacker_top.py' to path. This way users
    	   can just run 'hacker_top.py' (with tab completion) from anywhere on the
    	   system.

---
+++ setup.py
@@ -0,0 +1,34 @@
+"""Handles packaging, distribution, and testing."""
+
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup
+
+
+setup(
+
+	# Basic package information.
+	name = 'hacker-top',
+	version = '1.0',
+	scripts = ['hacker_top.py'],
+	py_modules = ['pyhackerstories'],
+
+	# Packaging options.
+	zip_safe = False,
+	include_package_data = True,
+
+	# Package dependencies.
+	install_requires = ['BeautifulSoup>=3.1.0.1'],
+
+	# Metadata for PyPI.
+	author = 'Peteris Krumins',
+	author_email = 'peter@catonmat.net',
+	license = 'GPL',
+	url = 'http://www.catonmat.net/blog/follow-hacker-news-from-the-console/',
+	keywords = 'hacker news cli',
+	description = 'A top-like program for monitoring stories on hacker news.',
+	long_description = open('readme.txt').read()
+
+)
