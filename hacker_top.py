1262152f71d92ed7619966f9eb1ab24f25e58e83
Nicolas Paris <nicolas.caen@gmail.com>
2011-06-14 19:13:44 +0200

    enable transparency

--- hacker_top.py
+++ hacker_top.py
@@ -356,11 +356,17 @@ class Interface(object):
         except:
             pass
 
-        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
-        curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
-        curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-        curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
+        try:
+            curses.use_default_colors()
+            bg = -1
+        except:
+            bg = curses.COLOR_BLACK
+
+        curses.init_pair(1, curses.COLOR_CYAN, bg)
+        curses.init_pair(2, curses.COLOR_WHITE, bg)
+        curses.init_pair(3, curses.COLOR_GREEN, bg)
+        curses.init_pair(4, curses.COLOR_YELLOW, bg)
+        curses.init_pair(5, curses.COLOR_RED, bg)
 
         self.max_y, self.max_x = stdscr.getmaxyx()
 
